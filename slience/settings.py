0c6e75ede5c29bac4ec0f636096362f71a3eaf38
PengfeiInPxn <yaopengfei@pxn.one>
2018-09-26 21:46:31 +0800

    task api 支持序列化crontab

--- slience/settings.py
+++ slience/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'slience',
         'USER':'root',
         'PASSWORD':'123456',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
     }
 }
--- taskManager/serialzers.py
+++ taskManager/serialzers.py
@@ -1,12 +1,22 @@
 from djcelery.models import PeriodicTask,CrontabSchedule
 from rest_framework import serializers
 
-class PeriodicTaskSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = PeriodicTask
-        fields = ('name','task','crontab','enabled','kwargs','queue','description')
-
 class CrontabScheduleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CrontabSchedule
         fields = ('minute','hour','day_of_week','day_of_month','month_of_year')
+
+
+#http://www.django-rest-framework.org/api-guide/serializers/#serializer-extensions
+class PeriodicTaskSerializer(serializers.HyperlinkedModelSerializer):
+    crontab = CrontabScheduleSerializer(many=False)
+
+    class Meta:
+        model = PeriodicTask
+        fields = ('name','task','crontab','enabled','kwargs','queue','description')
+
+    def create(self,validated_data):
+        crontab_data = validated_data.pop('crontab')
+        crontab = CrontabSchedule.objects.create(**crontab_data)
+        task = PeriodicTask.objects.create(crontab=crontab,**validated_data)
+        return task
